254d7caf78ea1589234402dbf5ae916fa5736b8f
강명구 <mg5566@hanmail.net>
2021-09-06 15:31:31 +0900

    solve 전화번호 목록

--- programmers_high_score_kit/전화번호 목록/main.cpp
+++ programmers_high_score_kit/전화번호 목록/main.cpp
@@ -1,5 +1,29 @@
 #include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+bool solution(vector<string> phone_book) {
+  bool answer = true;
+
+  sort(phone_book.begin(), phone_book.end());
+
+  for (int i = 0; i < (int)phone_book.size() - 1; ++i) {
+    // if (phone_book[i + 1].find(phone_book[i].c_str(), 0, phone_book[i].length()) != string::npos) {
+    // if (phone_book[i] == phone_book[i + 1].substr(0, phone_book[i].size())) {
+    if (phone_book[i + 1].compare(0, phone_book[i].length(), phone_book[i]) == 0) {
+      return false;
+    }
+  }
+  return answer;
+}
 
 int main(void) {
-	return (0);
+  vector<string> phone_book = {"12", "123", "1235", "567", "88"};
+  //vector<string> phone_book = {"123","456","789"};
+
+  cout << solution(phone_book) << endl;
+  return (0);
 }
