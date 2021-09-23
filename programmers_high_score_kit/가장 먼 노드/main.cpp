fd41ebdad4ca3facfbe295b69135523550e012b8
강명구 <mg5566@hanmail.net>
2021-09-23 17:07:39 +0900

    last update

---
+++ programmers_high_score_kit/가장 먼 노드/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int n, vector<vector<int>> edge) {
+  int answer = 0;
+  return answer;
+}
+
+int main(void) {
+  vector<vector<int>> vertax = {{3, 6}, {4, 3}, {3, 2}, {1, 3}, {1, 2}, {2, 4}, {5, 2}};
+
+  cout << solution(6, vertax) << endl;
+  return (0);
+}
